740ae9d3cba3f51b6611ad7406b2a5ee16c41bd2
eripahle <eripahle@gmail.com>
2015-02-27 15:01:23 +0700

    upload project pertama - bersih

---
+++ src/opencart/config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\program\xampp\htdocs\opencart/catalog/');
+define('DIR_SYSTEM', 'D:\program\xampp\htdocs\opencart/system/');
+define('DIR_LANGUAGE', 'D:\program\xampp\htdocs\opencart/catalog/language/');
+define('DIR_TEMPLATE', 'D:\program\xampp\htdocs\opencart/catalog/view/theme/');
+define('DIR_CONFIG', 'D:\program\xampp\htdocs\opencart/system/config/');
+define('DIR_IMAGE', 'D:\program\xampp\htdocs\opencart/image/');
+define('DIR_CACHE', 'D:\program\xampp\htdocs\opencart/system/cache/');
+define('DIR_DOWNLOAD', 'D:\program\xampp\htdocs\opencart/system/download/');
+define('DIR_UPLOAD', 'D:\program\xampp\htdocs\opencart/system/upload/');
+define('DIR_MODIFICATION', 'D:\program\xampp\htdocs\opencart/system/modification/');
+define('DIR_LOGS', 'D:\program\xampp\htdocs\opencart/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', 'oc_');
