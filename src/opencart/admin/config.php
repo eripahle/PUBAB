7103e6ece57d301ff04eab451384c43448b8224d
eripahle <eripahle@gmail.com>
2015-03-02 20:19:01 +0700

    ini ada perubahan Database. 1. mangga silakan disesuaikan lagi DB akan yg kemarin sorry conflict. lupa pull 2. udah ada modul FB/Twitter/Path lengkap + FB box like 3. layout sementara dummy seperti yang tertera 4. infotmasi "How To udah" tinggal Data asli gambarnya ganti

--- src/opencart/admin/config.php
+++ src/opencart/admin/config.php
@@ -1,25 +1,25 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://localhost/pubapp/src/opencart/admin/');
-define('HTTP_CATALOG', 'http://localhost/pubapp/src/opencart/');
+define('HTTP_SERVER', 'http://localhost/PUBAPP/src/opencart/admin/');
+define('HTTP_CATALOG', 'http://localhost/PUBAPP/src/opencart/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://localhost/pubapp/src/opencart/admin/');
-define('HTTPS_CATALOG', 'http://localhost/pubapp/src/opencart/');
+define('HTTPS_SERVER', 'http://localhost/PUBAPP/src/opencart/admin/');
+define('HTTPS_CATALOG', 'http://localhost/PUBAPP/src/opencart/');
 
 // DIR
-define('DIR_APPLICATION', 'C:\xampp\htdocs\pubapp\src\opencart/admin/');
-define('DIR_SYSTEM', 'C:\xampp\htdocs\pubapp\src\opencart/system/');
-define('DIR_LANGUAGE', 'C:\xampp\htdocs\pubapp\src\opencart/admin/language/');
-define('DIR_TEMPLATE', 'C:\xampp\htdocs\pubapp\src\opencart/admin/view/template/');
-define('DIR_CONFIG', 'C:\xampp\htdocs\pubapp\src\opencart/system/config/');
-define('DIR_IMAGE', 'C:\xampp\htdocs\pubapp\src\opencart/image/');
-define('DIR_CACHE', 'C:\xampp\htdocs\pubapp\src\opencart/system/cache/');
-define('DIR_DOWNLOAD', 'C:\xampp\htdocs\pubapp\src\opencart/system/download/');
-define('DIR_UPLOAD', 'C:\xampp\htdocs\pubapp\src\opencart/system/upload/');
-define('DIR_LOGS', 'C:\xampp\htdocs\pubapp\src\opencart/system/logs/');
-define('DIR_MODIFICATION', 'C:\xampp\htdocs\pubapp\src\opencart/system/modification/');
-define('DIR_CATALOG', 'C:\xampp\htdocs\pubapp\src\opencart/catalog/');
+define('DIR_APPLICATION', 'D:\program\xampp\htdocs\PUBAPP\src\opencart/admin/');
+define('DIR_SYSTEM', 'D:\program\xampp\htdocs\PUBAPP\src\opencart/system/');
+define('DIR_LANGUAGE', 'D:\program\xampp\htdocs\PUBAPP\src\opencart/admin/language/');
+define('DIR_TEMPLATE', 'D:\program\xampp\htdocs\PUBAPP\src\opencart/admin/view/template/');
+define('DIR_CONFIG', 'D:\program\xampp\htdocs\PUBAPP\src\opencart/system/config/');
+define('DIR_IMAGE', 'D:\program\xampp\htdocs\PUBAPP\src\opencart/image/');
+define('DIR_CACHE', 'D:\program\xampp\htdocs\PUBAPP\src\opencart/system/cache/');
+define('DIR_DOWNLOAD', 'D:\program\xampp\htdocs\PUBAPP\src\opencart/system/download/');
+define('DIR_UPLOAD', 'D:\program\xampp\htdocs\PUBAPP\src\opencart/system/upload/');
+define('DIR_LOGS', 'D:\program\xampp\htdocs\PUBAPP\src\opencart/system/logs/');
+define('DIR_MODIFICATION', 'D:\program\xampp\htdocs\PUBAPP\src\opencart/system/modification/');
+define('DIR_CATALOG', 'D:\program\xampp\htdocs\PUBAPP\src\opencart/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
