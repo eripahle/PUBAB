ff1aeddf00e66630b55ca1d6f14579139b18ea05
gifary <muhammadgifary@gmail.com>
2015-03-19 21:06:01 +0700

    penambahan config dan listeditbook.tpl

---
+++ src/opencart/admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/PUBAPP/src/opencart/admin/');
+define('HTTP_CATALOG', 'http://localhost/PUBAPP/src/opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/PUBAPP/src/opencart/admin/');
+define('HTTPS_CATALOG', 'http://localhost/PUBAPP/src/opencart/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\xampp\htdocs\PUBAPP\src\opencart/admin/');
+define('DIR_SYSTEM', 'C:\xampp\htdocs\PUBAPP\src\opencart/system/');
+define('DIR_LANGUAGE', 'C:\xampp\htdocs\PUBAPP\src\opencart/admin/language/');
+define('DIR_TEMPLATE', 'C:\xampp\htdocs\PUBAPP\src\opencart/admin/view/template/');
+define('DIR_CONFIG', 'C:\xampp\htdocs\PUBAPP\src\opencart/system/config/');
+define('DIR_IMAGE', 'C:\xampp\htdocs\PUBAPP\src\opencart/image/');
+define('DIR_CACHE', 'C:\xampp\htdocs\PUBAPP\src\opencart/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp\htdocs\PUBAPP\src\opencart/system/download/');
+define('DIR_UPLOAD', 'C:\xampp\htdocs\PUBAPP\src\opencart/system/upload/');
+define('DIR_LOGS', 'C:\xampp\htdocs\PUBAPP\src\opencart/system/logs/');
+define('DIR_MODIFICATION', 'C:\xampp\htdocs\PUBAPP\src\opencart/system/modification/');
+define('DIR_CATALOG', 'C:\xampp\htdocs\PUBAPP\src\opencart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', 'oc_');
---
+++ src/opencart/config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/PUBAPP/src/opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/PUBAPP/src/opencart/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\xampp\htdocs\PUBAPP\src\opencart/catalog/');
+define('DIR_SYSTEM', 'C:\xampp\htdocs\PUBAPP\src\opencart/system/');
+define('DIR_LANGUAGE', 'C:\xampp\htdocs\PUBAPP\src\opencart/catalog/language/');
+define('DIR_TEMPLATE', 'C:\xampp\htdocs\PUBAPP\src\opencart/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\xampp\htdocs\PUBAPP\src\opencart/system/config/');
+define('DIR_IMAGE', 'C:\xampp\htdocs\PUBAPP\src\opencart/image/');
+define('DIR_CACHE', 'C:\xampp\htdocs\PUBAPP\src\opencart/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp\htdocs\PUBAPP\src\opencart/system/download/');
+define('DIR_UPLOAD', 'C:\xampp\htdocs\PUBAPP\src\opencart/system/upload/');
+define('DIR_MODIFICATION', 'C:\xampp\htdocs\PUBAPP\src\opencart/system/modification/');
+define('DIR_LOGS', 'C:\xampp\htdocs\PUBAPP\src\opencart/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', 'oc_');
