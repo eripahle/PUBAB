e502dbbd38479e994fb91e1a029e7ebace08fae7
eripahle <eripahle@gmail.com>
2015-03-26 04:30:47 +0700

    Penambahan config file baru

--- src/opencart/catalog/controller/module/gallery.php
+++ src/opencart/catalog/controller/module/gallery.php
@@ -4,11 +4,13 @@ class ControllerModuleGallery extends Controller {
 		$this->load->language('module/gallery');
                 $this->load->model('module/gallery');
                 
+                $data['gallerys']= $this->model_module_gallery->getAllGallery();
+                
 		$data['heading_title'] = $this->language->get('heading_title');				 						
 			if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/module/gallery.tpl')) {
 				return $this->load->view($this->config->get('config_template') . '/template/module/gallery.tpl', $data);
 			} else {
-				return $this->load->view('default/template/module/mostreviewed.tpl', $data);
+				return $this->load->view('default/template/module/gallery.tpl', $data);
 			}
 		}
 }
