312af9927a23a8512db410475585370d8afdbc00
eripahle <eripahle@gmail.com>
2015-04-01 23:02:11 +0700

    Penambahan File baru untuk persiapan prototype appas

--- src/opencart/catalog/model/module/testimony.php
+++ src/opencart/catalog/model/module/testimony.php
@@ -6,14 +6,14 @@ class ModelModuleTestimony extends Model {
         $this->batas = 4;
 	$posisi = $p->cariPosisi($this->batas);	
 	
-        $sql = "SELECT * FROM " . DB_PREFIX . "review limit $posisi,$this->batas";
+        $sql = "SELECT * FROM " . DB_PREFIX . "review ORDER BY review_id DESC limit $posisi,$this->batas";
         $query = $this->db->query($sql);
         $testimonys = $query->rows;
         return $testimonys;
     }
     
     public function getLatestTestimony() {
-        $sql = "SELECT * FROM " . DB_PREFIX . "review limit 3";
+        $sql = "SELECT * FROM " . DB_PREFIX . "review ORDER BY review_id DESC limit 3 ";
         $query = $this->db->query($sql);
         $testimonys = $query->rows;
         return $testimonys;
